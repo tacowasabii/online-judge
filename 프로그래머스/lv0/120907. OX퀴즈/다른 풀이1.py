2320d7a3308e83812945006874f278a4b70ceeff
tacowasabii <87429658+tacowasabii@users.noreply.github.com>
2023-01-09 14:28:43 +0900

    Create 다른 풀이1.py

---
+++ 프로그래머스/lv0/120907. OX퀴즈/다른 풀이1.py
@@ -0,0 +1,6 @@
+def valid(equation):
+    equation = equation.replace('=', '==')
+    return eval(equation)
+
+def solution(equations):
+    return ["O" if valid(equation) else "X" for equation in equations]    
