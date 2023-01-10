d0ec575cbe747fab95b567519272783f842179c1
tacowasabii <87429658+tacowasabii@users.noreply.github.com>
2023-01-10 16:01:55 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,15 +1,16 @@
 def solution(keyinput, board):
-    answer = [0, 0]
     x, y = 0,0
     dx = [0, 0, -1, 1]
     dy = [1, -1, 0, 0]
     xl = board[0] // 2
     yl = board[1] // 2
     dir = ["up", "down", "left", "right"]
+    
     for i in keyinput:
         idx = dir.index(i)
         if xl >= x+dx[idx] >= -xl:
             x += dx[idx]
         if yl >= y+dy[idx] >= -yl:
             y += dy[idx]
+            
     return [x,y]
