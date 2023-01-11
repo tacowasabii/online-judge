d528717bd6fd2bcf9f7201e9dcd5700fbf191d08
tacowasabii <87429658+tacowasabii@users.noreply.github.com>
2023-01-11 20:40:11 +0900

    Update 완주하지 못한 선수.py

--- 프로그래머스/유형별 문제/해시/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/유형별 문제/해시/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,7 +1,13 @@
 def solution(participant, completion):
+    # 내 풀이
     a = dict.fromkeys(participant,0)
     for i in participant:
         a[i] += 1
+    # 모법 풀이
+    a = {}
+    for i in participant:
+        a[i] = a.get(i,0) + 1
+        
     for i in completion:
         a[i] -= 1
     b = [k for k, v in a.items() if v == 1]
