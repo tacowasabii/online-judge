187dbf9e7b6fb4ca8321088d4c2a4d38031cb0fd
tacowasabii <87429658+tacowasabii@users.noreply.github.com>
2023-01-15 14:49:32 +0900

    Update 안전지대.py

--- 프로그래머스/lv0/120866. 안전지대/안전지대.py
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.py
@@ -10,16 +10,12 @@ def solution(board):
                 for k in range(-1, 2):
                     for l in range(-1, 2):
                         a[i + k + 1][j + l + 1] = 1
-    
+                        
     a = a[1:-1]
     for j in range(len(a)):
         a[j] = a[j][1:-1]
-
     
     for i in a:
         answer += i.count(0)
 
     return answer
-
-
-
