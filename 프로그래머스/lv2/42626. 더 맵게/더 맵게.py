c44a700d03322e9f0dbab2562258b94c580f0b5f
tacowasabii <87429658+tacowasabii@users.noreply.github.com>
2023-01-27 16:14:22 +0900

    [level 2] Title: 더 맵게, Time: 1636.95 ms, Memory: 51.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -6,7 +6,7 @@ def solution(scoville, K):
     while True:
         if scoville[0] >= K:
             break
-        if len(scoville) == 1 and scoville[0] < K:
+        if len(scoville) == 1:
             return -1
         heapq.heappush(scoville, heapq.heappop(scoville) + heapq.heappop(scoville) * 2)
         answer += 1
