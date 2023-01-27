7f3d7dd5d7ce80d61748d410d0b650a210cef6ee
tacowasabii <godseoril812@gmail.com>
2023-01-27 16:20:25 +0900

    [level 2] Title: 더 맵게, Time: 1636.95 ms, Memory: 51.6 MB -BaekjoonHub

--- 프로그래머스/유형별 문제/힙/42626. 더 맵게/모범 풀이.py
+++ 프로그래머스/유형별 문제/힙/42626. 더 맵게/모범 풀이.py
@@ -3,14 +3,16 @@ import heapq
 def solution(scoville, K):
     answer = 0
     heapq.heapify(scoville)
-    while True:
-        min1 = heapq.heappop(scoville)
+    while True:  #O(n)
+        min1 = heapq.heappop(scoville)  #O(logn)
         if min1 >= K:
             break
         if len(scoville) == 1:
             return -1
         min2 = heapq.heappop(scoville)
-        new_scoville = min1 + 2 * min2
-        heapq.heappush(scoville, new_scoville)
+        new_scoville = min1 + 2 * min2  #O(logn)
+        heapq.heappush(scoville, new_scoville)   #O(logn)
         answer += 1
     return answer
+
+    #O(logn)
