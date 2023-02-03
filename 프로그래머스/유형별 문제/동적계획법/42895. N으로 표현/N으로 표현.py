4a0eee32ee882ae738c7ebea5fb41296818a91a7
tacowasabii <godseoril812@gmail.com>
2023-02-03 14:11:23 +0900

    [level 3] Title: N으로 표현, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/유형별 문제/동적계획법/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/유형별 문제/동적계획법/42895. N으로 표현/N으로 표현.py
@@ -1,19 +1,19 @@
 def solution(N, number):
     answer = 1
-    a = [[],[N]]
+    a = [[], [N]]
     while answer <= 8:
         if number in a[answer]:
             return answer
-        answer+=1
+        answer += 1
         temp = []
-        temp.append(int(str(N)*answer))
-        for i in range(1,len(a)):
+        temp.append(int(str(N) * answer))
+        for i in range(1, len(a)):
             for j in a[i]:
-                for k in a[len(a)-i]:
-                    temp.append(j+k)
+                for k in a[len(a) - i]:
+                    temp.append(j + k)
                     temp.append(j - k)
                     temp.append(j * k)
-                    if k!=0:
+                    if k != 0:
                         temp.append(j // k)
         a.append(list(set(temp)))
 
