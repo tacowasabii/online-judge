577b14fdd2a921184a107e1cd75915a84f8425fc
tacowasabii <87429658+tacowasabii@users.noreply.github.com>
2023-02-03 13:53:39 +0900

    [level 3] Title: N으로 표현, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -0,0 +1,20 @@
+def solution(N, number):
+    answer = 1
+    a = [[],[N]]
+    while answer <= 8:
+        if number in a[answer]:
+            return answer
+        answer+=1
+        temp = []
+        temp.append(int(str(N)*answer))
+        for i in range(1,len(a)):
+            for j in a[i]:
+                for k in a[len(a)-i]:
+                    temp.append(j+k)
+                    temp.append(j - k)
+                    temp.append(j * k)
+                    if k!=0:
+                        temp.append(j // k)
+        a.append(list(set(temp)))
+
+    return -1
