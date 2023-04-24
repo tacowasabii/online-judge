ec9d0a1dbc73beafb4b4855e576bd0d600ae5057
tacowasabii <godseoril812@gmail.com>
2023-04-24 15:01:42 +0900

    [Bronze III] Title: 약수 구하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+a = []
+for i in range(1, n + 1):
+    if n % i == 0:
+        a.append(i)
+if len(a) >= k:
+    print(a[k - 1])
+else:
+    print(0)
