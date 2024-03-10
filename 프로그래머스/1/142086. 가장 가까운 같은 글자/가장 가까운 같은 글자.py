42bc7b6072eb4486fb100f5e840d6c3705d467e4
tacowasabii <godseoril812@gmail.com>
2024-03-10 23:43:35 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 1.56 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = []
+    dic = {}
+    for i, v in enumerate(s):
+        if v not in dic:
+            answer.append(-1)
+        else:
+            answer.append(i - dic[v])
+        dic[v] = i
+    return answer
