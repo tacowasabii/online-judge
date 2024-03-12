44d63e0b3657235494830e89c1b9be0a67a3c5d9
tacowasabii <godseoril812@gmail.com>
2024-03-12 15:11:32 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,14 +1,7 @@
-def isPrime(n):
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
 def solution(nums):
-    answer = 0
-    for i in range(len(nums)-2):
-        for j in range(i+1, len(nums)-1):
-            for k in range(j+1, len(nums)):
-                if isPrime(nums[i]+nums[j]+nums[k]):
-                    answer +=1
+    answer = -1
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
     return answer
