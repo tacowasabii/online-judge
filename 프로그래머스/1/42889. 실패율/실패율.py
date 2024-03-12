0ec0d45375d4e56cae616ffa96c6740edcff963f
tacowasabii <godseoril812@gmail.com>
2024-03-12 17:44:01 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -0,0 +1,10 @@
+def solution(N, stages):
+    total = len(stages)
+    dic = {}
+    for i in sorted(list(set(stages))):
+        if i <= N:
+            dic[i] = stages.count(i) / total
+            total -= stages.count(i)
+    for i in range(1, N+1):
+        dic[i] = dic.get(i,0)
+    return sorted(dic, key=lambda x: dic[x], reverse=True)
