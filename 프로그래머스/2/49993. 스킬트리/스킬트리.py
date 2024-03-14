97fa724f43b24dc0afa1d15eadfc94f9ef51dcb7
tacowasabii <godseoril812@gmail.com>
2024-03-14 14:34:01 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -0,0 +1,11 @@
+def solution(skill, skill_trees):
+    answer = 0
+    
+    for i in skill_trees:
+        tmp = i
+        for j in i:
+            if j not in skill:
+                tmp = tmp.replace(j,'')
+        if skill.startswith(tmp):
+            answer += 1
+    return answer
