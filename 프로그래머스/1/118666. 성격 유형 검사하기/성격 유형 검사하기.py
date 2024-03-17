7af60592dadfb0a4d4736516228ef3f43bddd6ee
tacowasabii <godseoril812@gmail.com>
2024-03-17 10:12:18 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,27 @@
+def solution(survey, choices):
+    answer = ''
+    dic = {}
+    for i, v in enumerate(survey):
+        choice = choices[i]
+        if choice == 1:
+            dic[v[0]] = dic.get(v[0],0) + 3
+        elif choice == 2:
+            dic[v[0]] = dic.get(v[0],0) + 2
+        elif choice == 3:
+            dic[v[0]] = dic.get(v[0],0) + 1
+        elif choice == 5:
+            dic[v[1]] = dic.get(v[1],0) + 1
+        elif choice == 6:
+            dic[v[1]] = dic.get(v[1],0) + 2
+        elif choice == 7:
+            dic[v[1]] = dic.get(v[1],0) + 3
+            
+    per = [("R", "T"),("C", "F"),("J","M"),("A","N")]
+    
+    for i in per:
+        if dic.get(i[0],0) >= dic.get(i[1],0):
+            answer += i[0]
+        else:
+            answer += i[1]
+    
+    return answer
