422260bcf178164c0bfcdfd17d737f26cfab4baf
tacowasabii <godseoril812@gmail.com>
2024-03-17 14:08:50 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.35 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,19 @@
+def solution(new_id):
+    new_id = new_id.lower()
+    tmp = []
+    for i in new_id:
+        if ord('a') <= ord(i) <= ord('z') or i.isdigit() or i =='-' or i =='_' or i=='.':
+            tmp.append(i)
+    for i in range(len(tmp) - 1):
+        if tmp[i] == '.' and tmp[i+1] == '.':
+            tmp[i] = ''
+    tmp = ''.join(tmp)
+    tmp = tmp.strip('.')
+    if not tmp:
+        tmp += 'a'
+    new_id = tmp[:15]
+    new_id = new_id.rstrip('.')
+    
+    while len(new_id) < 3:
+        new_id += new_id[-1]
+    return new_id
