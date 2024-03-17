1fde5a65bf16f24378397c8669484f2f3ecc4f91
tacowasabii <godseoril812@gmail.com>
2024-03-17 20:04:46 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,27 @@
+import heapq
+
+def solution(operations):
+    min_heap = []
+    max_heap = []
+    
+    for i in operations:
+        command, value = i.split()
+        value = int(value)
+        
+        if command == "I":
+            heapq.heappush(min_heap,value)
+            heapq.heappush(max_heap,-value)
+        else:
+            if value == 1 and max_heap:
+                max_value = heapq.heappop(max_heap)
+                min_heap.remove(-max_value)
+            elif value == -1 and min_heap: 
+                min_value = heapq.heappop(min_heap)
+                max_heap.remove(-min_value)
+    
+    if min_heap:
+        return [-max_heap[0],min_heap[0]]
+    return [0,0]
+            
+    
+    return answer
