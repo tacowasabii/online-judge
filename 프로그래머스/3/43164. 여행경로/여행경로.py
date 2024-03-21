d5825e9ddc8e75a0b1aad3c1d8f094318a1a7a1d
tacowasabii <godseoril812@gmail.com>
2024-03-21 13:37:37 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -0,0 +1,24 @@
+from collections import deque
+from collections import defaultdict
+
+def solution(tickets):
+    answer = []
+    
+    dic = defaultdict(list)
+    for start, arrive in tickets:
+        dic[start].append(arrive)
+    for i in dic:
+        dic[i].sort(reverse = True)
+    
+    stack = ["ICN"]
+    
+    while stack:
+        tmp = stack[-1]
+        
+        if tmp in dic and len(dic[tmp]) > 0:
+            stack.append(dic[tmp].pop())
+        else:
+            answer.append(tmp)
+            stack.pop()
+    
+    return answer[::-1]
