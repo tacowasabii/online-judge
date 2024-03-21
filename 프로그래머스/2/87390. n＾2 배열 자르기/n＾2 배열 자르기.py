515c5341b2b7cd5452d52aea74bc9985a2f73992
tacowasabii <godseoril812@gmail.com>
2024-03-21 17:39:03 +0900

    [level 2] Title: n^2 배열 자르기, Time: 33.09 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,7 +1,9 @@
 def solution(n, left, right):
     answer = []
+    
     for i in range(left, right + 1):
-        row = i // n
-        col = i % n
-        answer.append(max(row, col) + 1)
+        row = i // n + 1
+        col = i % n + 1
+        answer.append(max(row,col))
+        
     return answer
