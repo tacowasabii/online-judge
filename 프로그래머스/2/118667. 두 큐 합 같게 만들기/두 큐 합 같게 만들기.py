a8e045ee6ed5f7edca95848a205909249d5374cf
tacowasabii <godseoril812@gmail.com>
2024-03-23 00:10:24 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 111.03 ms, Memory: 21.2 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -2,21 +2,24 @@ def solution(queue1, queue2):
     total_sum = sum(queue1) + sum(queue2)
     if total_sum % 2 != 0: 
         return -1
-    target = total_sum // 2
-    len_q1, len_q2 = len(queue1), len(queue2)
-    idx1, idx2, current_sum, count = 0, len_q1, sum(queue1), 0
-    queue = queue1 + queue2 + queue1  
-
-    while idx1 < len_q1+len_q2 and idx2 < len_q1*2+len_q2:
-        if current_sum == target:
-            return count
-        elif current_sum < target:
-            current_sum += queue[idx2]
+    
+    idx1, idx2 = 0, len(queue1)
+    queue = queue1 + queue2 + queue1
+    sum1, sum2 = sum(queue1), sum(queue2)
+    cnt = 0
+    
+    while idx1 < len(queue1) + len(queue2) and idx2 < len(queue):
+        if sum1 == total_sum / 2:
+            return cnt
+        elif sum1 < sum2:
+            sum1 += queue[idx2]
+            sum2 -= queue[idx2]
             idx2 += 1
-            count += 1
-        else:
-            current_sum -= queue[idx1]
+            cnt += 1
+        elif sum1 > sum2:
+            sum1 -= queue[idx1]
+            sum2 += queue[idx1]
             idx1 += 1
-            count += 1
-
+            cnt += 1
+            
     return -1
