ea3a7abb69073d340aff9ea25341c2fcfd45e860
tacowasabii <godseoril812@gmail.com>
2024-03-22 20:36:26 +0900

    [level 1] Title: 옹알이 (2), Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,19 +1,16 @@
 def solution(babbling):
-    pr = ["aya", "ye", "woo", "ma"]
+    prs = ["aya", "ye", "woo", "ma"]
     cnt = 0
     for i in babbling:
-        flag = 1
         tmp = ''
-        while len(i) > 0 and flag == 1:
-            flag = 0
-            for j in pr:
-                if j != tmp and i.startswith(j):
-                    i = i[len(j):]
-                    tmp = j
-                    flag = 1
-                    continue
-            if len(i) == 0:
-                cnt += 1
-            if flag == 0:
+        while i:
+            length = len(i)
+            for pr in prs:
+                if i.startswith(pr) and pr != tmp:
+                    tmp = pr
+                    i = i[len(pr):]
+            if length == len(i):
                 break
+        if not i:
+            cnt += 1
     return cnt
