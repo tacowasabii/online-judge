619812351036f00fb27efd53c89c5a381da03c0a
tacowasabii <godseoril812@gmail.com>
2024-09-20 22:05:16 +0900

    [level 2] Title: 구명보트, Time: 9.62 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,13 +1,13 @@
 from collections import deque
 
 def solution(people, limit):
-    people.sort(reverse = True)
-    people = deque(people)
     ans = 0
+    people.sort()
+    people = deque(people)
     while people:
-        tmp = people.popleft()
-        if people and tmp + people[-1] <= limit:
-            people.pop()
+        weight = 0
+        weight += people.pop()
+        if people and people[0] + weight <= limit:
+            people.popleft()
         ans += 1
-
     return ans
