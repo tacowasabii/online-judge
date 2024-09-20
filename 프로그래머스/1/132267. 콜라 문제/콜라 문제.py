8a0f3d40b6508b5ccc5544fe32556dc94bfdfa0d
tacowasabii <godseoril812@gmail.com>
2024-09-20 22:27:54 +0900

    [level 1] Title: 콜라 문제, Time: 0.45 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,6 +1,8 @@
+import math
+
 def solution(a, b, n):
     answer = 0
     while n >= a:
-        answer += b * (n // a)
+        answer += (n // a) * b
         n = (n // a) * b + n % a
     return answer
