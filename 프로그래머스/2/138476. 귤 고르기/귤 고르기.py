7dcfcb901a8be94f5b0f9817dbf6a0c2d9598e86
tacowasabii <godseoril812@gmail.com>
2024-09-21 00:10:52 +0900

    [level 2] Title: 귤 고르기, Time: 21.34 ms, Memory: 21.8 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,13 +1,11 @@
 from collections import Counter
 
 def solution(k, tangerine):
-    answer = 0
-    fruits = Counter(tangerine)
-    nums = list(fruits.values())
-    nums.sort()
-    
-    total = 0
-    while total < k:
-        total += nums.pop()
-        answer += 1
-    return answer
+    ans = 0
+    oranges = sorted(Counter(tangerine).values(), reverse = True)
+
+    for i in oranges:
+        k -= i
+        ans += 1
+        if k <= 0:
+            return ans
