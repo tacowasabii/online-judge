5c34162d69f60f62f1ddc0c196cd4dfa75af9e8d
tacowasabii <godseoril812@gmail.com>
2024-09-22 20:56:38 +0900

    [level 2] Title: 괄호 회전하기, Time: 92.68 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,21 +1,21 @@
+def isPossible(s):
+    stack = []
+    for i in s:
+        if i == "(" or i == "{" or i == "[":
+            stack.append(i)
+        elif stack:
+            if i == ")" and stack[-1] == "(" or i == "}" and stack[-1] == "{" or i == "]" and stack[-1] == "[":
+                stack.pop()
+            else:
+                return False
+        else:
+            return False
+    return len(stack) == 0
+
 def solution(s):
     answer = 0
-    for _ in range(len(s)):
-        stack = []
-        flag = 0
-        for i in s:
-            if i == '(' or i == '{' or i == '[':
-                stack.append(i)
-            else:
-                if len(stack) == 0:
-                    flag = 1
-                else:
-                    if (i == ']' and stack[-1] == '[') or  (i == ')' and stack[-1] == '(') or  (i == '}' and stack[-1] == '{'):
-                        stack.pop()
-                        
-        if len(stack) == 0 and flag == 0:
+    
+    for i in range(len(s)):
+        if isPossible(s[i:] + s[:i]):
             answer += 1
-            
-        s = s[1:] + s[0]
-     
     return answer
