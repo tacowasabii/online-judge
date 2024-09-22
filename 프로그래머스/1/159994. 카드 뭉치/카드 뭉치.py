62874c8b95b1aca4c169077377581802c23d6a33
tacowasabii <godseoril812@gmail.com>
2024-09-22 20:15:27 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,11 +1,14 @@
+from collections import deque
+
 def solution(cards1, cards2, goal):
-    answer = ''
+    cards1 = deque(cards1)
+    cards2 = deque(cards2)
     
     for i in goal:
-        if len(cards1) > 0 and i == cards1[0]:
-            cards1.pop(0)
-        elif len(cards2) > 0 and i == cards2[0]:
-            cards2.pop(0)
+        if cards1 and i == cards1[0]:
+            cards1.popleft()
+        elif cards2 and i == cards2[0]:
+            cards2.popleft()
         else:
             return "No"
     return "Yes"
