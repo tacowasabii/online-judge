8969aa2221540826b3a6bf18fcfc5125fa145c4d
tacowasabii <godseoril812@gmail.com>
2024-09-23 01:16:10 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -2,7 +2,7 @@ def solution(clothes):
     answer = 1
     dic = {}
     for i in clothes:
-        dic[i[1]] = dic.get(i[1],0) + 1
-    for v in dic.values():
-        answer *= v + 1
+        dic[i[1]] = dic.get(i[1], 0) + 1
+    for i in dic.values():
+        answer *= i + 1
     return answer - 1
