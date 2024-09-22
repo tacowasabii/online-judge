2d1cf511c19cc5c921bc31052f577a45100b1fe7
tacowasabii <godseoril812@gmail.com>
2024-09-23 00:34:05 +0900

    [level 2] Title: 할인 행사, Time: 173.89 ms, Memory: 17.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -7,11 +7,7 @@ def solution(want, number, discount):
         dic[v] = number[i]
     
     for i in range(len(discount) - sum(number) + 1):
-        discount_dict = Counter(discount[i:i + sum(number)])
-        flag = True
-        for j in want:
-            if dic[j] != discount_dict[j]:
-                flag = False
-        if flag:
+        if dic == Counter(discount[i:i + sum(number)]):
             answer += 1
+
     return answer
