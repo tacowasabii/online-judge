840cf3955429a716d24799c0ab67e64c57d3269b
tacowasabii <godseoril812@gmail.com>
2024-09-23 12:13:08 +0900

    [level 2] Title: H-Index, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,10 +1,6 @@
 def solution(citations):
-    citations.sort(reverse=True)
-    answer = 0
-    
-    for i, citation in enumerate(citations):
-        if citation >= i + 1:
-            answer = i + 1
-        else:
-            break
-    return answer
+    citations.sort()
+    for i, v in enumerate(citations):
+        if len(citations) - i <= v:
+            return len(citations) - i
+    return 0
