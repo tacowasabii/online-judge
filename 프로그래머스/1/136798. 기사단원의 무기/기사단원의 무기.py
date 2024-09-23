1598b6298a2f8e9b17bf4155200e874608182e36
tacowasabii <godseoril812@gmail.com>
2024-09-23 15:14:39 +0900

    [level 1] Title: 기사단원의 무기, Time: 1867.42 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,20 +1,17 @@
-def getDivisorNum(n):
-    num = 0
-    
-    for i in range(1, int(n**0.5) +1):
-        if n % i == 0:
-            num += 1
-            if i ** 2 != n:
-                num +=1
-    return num
+def nums(num):
+    cnt = 0
+    for i in range(1, int(num ** 0.5) + 1):
+        if num % i == 0:
+            cnt += 2
+    if num ** 0.5 == int(num ** 0.5):
+        cnt -= 1
+    return cnt
 
 def solution(number, limit, power):
     answer = 0
-    tmp = []
-    for i in range(1, number+1):
-        tmp.append(getDivisorNum(i))
-    for i, v in enumerate(tmp):
-        if v > limit:
-            tmp[i] = power
-            
-    return sum(tmp)
+    for i in range(1, number + 1):
+        if nums(i) > limit:
+            answer += power
+        else:
+            answer += nums(i)
+    return answer
