df2d5dd01927406e6e832a71d8662991f7f6fac8
tacowasabii <godseoril812@gmail.com>
2024-09-24 15:32:38 +0900

    [level 2] Title: 피로도, Time: 41.35 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,13 +1,12 @@
 def solution(k, dungeons):
     answer = [0]
     
-    def explore(k, dungeons, num):
-        for i, v in enumerate(dungeons):
-            if k >= v[0]:
+    def explore(dungeons, blood, num):
+        for i in range(len(dungeons)):
+            if blood >= dungeons[i][0]:
                 answer[0] = max(answer[0], num + 1)
-                explore(k - v[1], dungeons[:i]+dungeons[i+1:], num + 1)
-    
-    explore(k, dungeons, 0)
+                explore(dungeons[:i] + dungeons[i + 1:], blood - dungeons[i][1], num + 1)
+    explore(dungeons, k, 0)
     
     return answer[0]
         
