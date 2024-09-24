dd5322f45e9d5006cfd13221ce5cbfb64162d0b5
tacowasabii <godseoril812@gmail.com>
2024-09-24 21:51:51 +0900

    [level 1] Title: 소수 찾기, Time: 339.46 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,11 +1,19 @@
-def solution(n):
-    ans = 0
-    arr = [True] * (n + 1)
-    for i in range(2, n + 1):
-        for j in range(2, n // i + 1):
-            arr[i * j] = False
-    for i in arr[2:]:
-        if i:
-            ans += 1
+# def solution(n):
+#     ans = 0
+#     arr = [True] * (n + 1)
+#     for i in range(2, n + 1):
+#         for j in range(2, n // i + 1):
+#             arr[i * j] = False
+#     for i in arr[2:]:
+#         if i:
+#             ans += 1
 
-    return ans
+#     return ans
+
+def solution(n):
+    num = set(range(2, n + 1))
+
+    for i in range(2, n + 1):
+        if i in num:
+            num -= set(range(2 * i, n + 1,i))
+    return len(num)
