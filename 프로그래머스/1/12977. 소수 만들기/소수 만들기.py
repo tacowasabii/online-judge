07b6db0e6da336ef0795d1587f8ad1a140de01d2
tacowasabii <godseoril812@gmail.com>
2024-09-24 15:36:45 +0900

    [level 1] Title: 소수 만들기, Time: 25.97 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,14 +1,14 @@
 from itertools import combinations as cb
 
-def isPrime(n):
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+def isPrime(num):
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
             return False
     return True
 
 def solution(nums):
     answer = 0
-    for i in cb(nums,3):
+    for i in list(cb(nums, 3)):
         if isPrime(sum(i)):
             answer += 1
     return answer
