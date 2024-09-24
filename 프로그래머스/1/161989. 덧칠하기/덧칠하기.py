86c8bf063ab5b9221e6aee14f4c995dbe86d28f6
tacowasabii <godseoril812@gmail.com>
2024-09-24 22:09:34 +0900

    [level 1] Title: 덧칠하기, Time: 7.11 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,8 +1,8 @@
 def solution(n, m, section):
-    answer = 1
-    start = section[0]
+    point = 0
+    ans = 0
     for i in section:
-        if i >= start + m:
-            answer += 1
-            start = i
-    return answer
+        if i > point:
+            point = i + m - 1
+            ans += 1
+    return ans
