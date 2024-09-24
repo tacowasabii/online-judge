d5da31c7b06bb10fd4da4fd23579f5f984c244d1
tacowasabii <godseoril812@gmail.com>
2024-09-24 19:44:01 +0900

    [level 2] Title: 프로세스, Time: 0.68 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,21 +1,16 @@
 from collections import deque
 
 def solution(priorities, location):
-    queue = deque(priorities)
-    cnt = 0
-    while True:
-        a = queue.popleft()
-        if len(queue) == 0:
-            return cnt + 1
-        if a < max(queue):
-            queue.append(a)
-            if location == 0:
-                location += len(queue) - 1
-            else:
-                location -= 1
+    order = 0
+    priorities = deque(priorities)
+    while location >= 0:
+        tmp = priorities.popleft()
+        location -= 1
+        if priorities and tmp < max(priorities):
+            priorities.append(tmp)
+            if location == -1:
+                location = len(priorities) - 1
         else:
-            if location == 0:
-                return cnt + 1
-            else:
-                location -= 1
-                cnt += 1
+            order += 1
+    return order            
+                
