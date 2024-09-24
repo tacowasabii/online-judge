a2f3c697390b39e5eafb77d744bba8f28d9fcfdd
tacowasabii <godseoril812@gmail.com>
2024-09-24 21:23:39 +0900

    [level 1] Title: 과일 장수, Time: 178.55 ms, Memory: 21.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,7 +1,9 @@
 def solution(k, m, score):
     answer = 0
+    if len(score) < m:
+        return 0
     score.sort(reverse = True)
-
-    for i in range(len(score)//m):
-        answer += min(score[i*m:i*m+m]) * m
+    for i in range(len(score) // m):
+        arr = score[i * m:(i + 1) * m]
+        answer += min(arr) * m
     return answer
