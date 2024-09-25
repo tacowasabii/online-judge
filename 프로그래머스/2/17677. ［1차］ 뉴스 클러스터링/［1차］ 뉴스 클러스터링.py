0004846a3f5da8fa2dc67dc9f6b9392fd48d14ca
tacowasabii <godseoril812@gmail.com>
2024-09-25 14:46:15 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.75 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,34 +1,34 @@
-def isAlph(str):
-    if ord('a') <= ord(str) <= ord('z'):
-        return True
-
 def solution(str1, str2):
     str1 = str1.lower()
     str2 = str2.lower()
-    s1 = []
-    s2 = []
-    for i in range(len(str1)-1):
-        if isAlph(str1[i]) and isAlph(str1[i+1]):
-            s1.append(str1[i:i+2])
-    for i in range(len(str2)-1):
-        if isAlph(str2[i]) and isAlph(str2[i+1]):
-            s2.append(str2[i:i+2])      
     
-    if len(s1) == 0 and len(s2) == 0:
-        return 65536
-    intersection = 0
+    set1 = set()
+    set2 = set()
+    dict1 = {}
+    dict2 = {}
     
-    for i in list(set(s1)):
-        if i in s2:
-            intersection += min(s1.count(i), s2.count(i))
+    for i in range(len(str1) - 1):
+        if str1[i:i + 2].isalpha():
+            set1.add(str1[i:i + 2])
+            dict1[str1[i:i + 2]] = dict1.get(str1[i:i + 2], 0) + 1
             
-    union = 0
-    for i in list(set(s1)):
-        if i in s2:
-            union += max(s1.count(i), s2.count(i))
-        else:
-            union += s1.count(i)
-    for i in list(set(s2)):
-        if i not in s1:
-            union += s2.count(i)
-    return int((intersection / union) * 65536)
+    for i in range(len(str2) - 1):
+        if str2[i:i + 2].isalpha():
+            set2.add(str2[i:i + 2])
+            dict2[str2[i:i + 2]] = dict2.get(str2[i:i + 2], 0) + 1
+    
+    if len(set1) == 0 and len(set2) == 0:
+        return 65536
+    
+    union = set1 | set2
+    intersection = set1 & set2
+    
+    cnt1 = 0
+    cnt2 = 0
+    
+    for i in intersection:
+        cnt1 += min(dict1[i], dict2[i])
+    for i in union:
+        cnt2 += max(dict1.get(i, 0), dict2.get(i, 0))
+    
+    return int(cnt1 / cnt2 * 65536)
