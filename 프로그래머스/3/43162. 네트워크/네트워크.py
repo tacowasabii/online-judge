f03172c2ed53bfd296a74d370e34be6ef340f298
tacowasabii <godseoril812@gmail.com>
2024-10-01 18:51:13 +0900

    [level 3] Title: 네트워크, Time: 1.63 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,16 @@
 def solution(n, computers):
+    ans = 0
     visited = [0] * n
-    network_count = 0
     
     for i in range(n):
         if visited[i] == 0:
+            ans += 1
             stack = [i]
-            
             while stack:
-                node = stack.pop()
-                # if not visited[node]:
-                visited[node] = 1
-                for j, v in enumerate(computers[node]):
-                    if v == 1 and not visited[j]:
+                point = stack.pop()
+                visited[point] = 1
+                for j, v in enumerate(computers[point]):
+                    if visited[j] == 0 and j != point and v == 1:
                         stack.append(j)
-            network_count += 1
-    
-    return network_count
+        
+    return ans
