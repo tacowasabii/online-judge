9c616dfd0e36375ea0b59cf5b5400f326a0ceca3
tacowasabii <godseoril812@gmail.com>
2024-10-01 19:05:14 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 98.42 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,23 +1,25 @@
-def TentoK(n, k):
-    tmp = ''
-    while n:
-        tmp += str(n % k)
-        n //= k
-    return tmp[::-1]
-
-def isPrime(n):
-    if n == 1:
+def isPrime(num):
+    if num == 1:
         return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
             return False
     return True
 
+def tenToK(n, k):
+    tmp = []
+    while n > 0:
+        tmp.append(str(n % k))
+        n //= k
+    return ''.join(tmp[::-1])
+
 def solution(n, k):
-    toK = TentoK(n,k)
-    tmp = toK.split("0")
-    cnt = 0
-    for i in tmp:
-        if i !='' and isPrime(int(i)):
-            cnt += 1
-    return cnt
+    ans = 0
+    nums = list(map(int, filter(None, tenToK(n, k).split("0"))))
+
+    for i in nums:
+        if isPrime(i):
+            ans += 1
+    
+    return ans
+    
