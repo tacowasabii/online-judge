e90421434b1d389e3cf1654042e7c553681a76f4
tacowasabii <godseoril812@gmail.com>
2024-10-03 01:26:33 +0900

    [level 2] Title: 방문 길이, Time: 3.43 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -2,15 +2,15 @@ def solution(dirs):
     answer = 0
     paths = []
     cur = (0,0)
-    dir = {"L":(-1,0), "R":(1,0), "U":(0,1), "D":(0,-1)}
+    dic = {'U':(0, 1), "D":(0, -1), "R":(1, 0), "L":(-1, 0)}
     
     for i in dirs:
-        nx = cur[0] + dir[i][0]
-        ny = cur[1] + dir[i][1]
+        nx = cur[0] + dic[i][0]
+        ny = cur[1] + dic[i][1]
         if -5 <= nx <= 5 and -5 <= ny <= 5:
-            path = (cur,(nx,ny))
-            if path not in paths:
+            if [cur, (nx, ny)] not in paths:
                 answer += 1
-                paths += [(cur,(nx,ny)),((nx,ny),cur)]
-            cur = (nx,ny)
+                paths += [[cur, (nx, ny)], [(nx, ny), cur]]
+            cur = (nx, ny)
+    
     return answer
