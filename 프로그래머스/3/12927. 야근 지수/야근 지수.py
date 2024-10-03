3f9524e73c4ae20fa25e51fd13d5ceab38f2ea13
tacowasabii <godseoril812@gmail.com>
2024-10-03 14:53:58 +0900

    [level 3] Title: 야근 지수, Time: 22.34 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,29 +1,22 @@
 from collections import Counter
 
 def solution(n, works):
-    answer = 0
-    max_work = max(works)
-    works_counter = Counter(sorted(works, reverse = True))
-
-    while max_work > 0 and n > 0:
-        max_n = works_counter[max_work]
-        if max_n >= n:
-            if max_work == 1:
-                return max_n - n
-            else:
-                works_counter[max_work] -= n
-                works_counter[max_work-1] += n
-                break
+    result = 0
+    max_ = max(works)
+    dic = Counter(works)
+    
+    while n > 0:
+        if dic[max_] >= n:
+            dic[max_] -= n
+            dic[max_ - 1] += n
+            n = 0
         else:
-            if max_work == 1:
-                return 0
-            else:
-                works_counter[max_work-1] += works_counter[max_work]
-                n -= works_counter[max_work]
-                del works_counter[max_work]
-        max_work -= 1
-        
-    for k, v in works_counter.items():
-        answer += k ** 2 * v
-                
-    return answer
+            dic[max_ - 1] += dic[max_]
+            n -= dic[max_]
+            del dic[max_]
+        max_ -= 1
+    
+    for i in range(1, max(dic) + 1):
+        result += i ** 2 * dic[i]
+    
+    return result
